05d3b1a666872034a03935fe7b32da53ab0884e0
Adarsh UV <adarshuv2028@cs.sjcetpalai.ac.in>
2025-07-30 21:51:33 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ class Student {
      char grade;
 
     public void displayDetails(){
-         System.out.println"Name: " + name);
-         System.out.println"Roll Number: " + rollNumber);
+         System.out.println("Name: " + name);
+         System.out.println("Roll Number: " + rollNumber);
          System.out.println("Grade: " + grade);
     }
 }
